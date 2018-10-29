510d6bd4314d6afe9799cb8e2bb82761f666e0a1
Amritraj <amritrjakacabu015@gmail.com>
2018-10-28 23:49:17 -0400

    First commit: 1) Added game assets. 2) Created the initial background scene. 3) Added initial card function

---
+++ Assets/scripts/TwoC.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoC : MonoBehaviour
+{
+
+    [SerializeField] private GameObject green_back;
+
+    public void OnMouseDown()
+    {
+        if (green_back.activeSelf)
+        {
+            green_back.SetActive(false);
+        }
+    }
+}
